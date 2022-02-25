d0c6a48ecbb8d0f3074cbc272f8413061fe4a04e
Asia K <66960776+asiakay@users.noreply.github.com>
2022-02-25 12:57:28 +0000

    Initial commit

---
+++ thankyou.php
@@ -0,0 +1,14 @@
+<html>
+  <body>
+    Thank you for being in touch. <p>
+      Here is the information you submitted.
+      
+    </p>
+    <p>
+      <?php echo $_POST["name"]; ?>
+<br>
+      <?php echo $_POST["email"]; ?>
+
+    </p>
+  </body>
+</html>
